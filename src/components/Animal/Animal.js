fc4be7cda05e33e9b642fbfe3488afe00f016c4b
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-09-12 14:32:24 -0700

    added animal and main function

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,13 @@
-export default function Animal() {
-  return <></>;
+import React from 'react';
+
+
+export default function Animal(props) {
+  return (
+    <div>
+      <span>{props.name}</span>
+      <span>{props.type}</span>
+      <span>{props.says}</span>
+    </div>
+  );
 }
+
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import animals from '../../data.js';
+import Animal from '../Animal/Animal.js';
 import './Main.css';
+import backgroundImg from '../../background.png';
 
 export default function Main() {
-  return <main></main>;
+  <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+    {animals.map((animal) => (
+      <Animal 
+        key={animal.name}
+      />
+    ))}
+  </main>;
 }
